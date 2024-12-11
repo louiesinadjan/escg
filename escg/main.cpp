73e0ec4c97d71d065139e32499806e961141b587
Louie Sinadjan <louie_sinz@yahoo.co.uk>
2024-12-11 17:25:51 +0000

    Genesis

--- escg/main.cpp
+++ escg/main.cpp
@@ -5,10 +5,19 @@
 //  Created by Louie Sinadjan on 11/12/2024.
 //
 
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+    
 }
