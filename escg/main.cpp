2f30dfaf62d03e43044c9cc4613c094f015eda50
Louie Sinadjan <bn22907@bristol.ac.uk>
2024-12-11 16:54:50 +0000

    Initial Commit

---
+++ escg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  escg
+//
+//  Created by Louie Sinadjan on 11/12/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
