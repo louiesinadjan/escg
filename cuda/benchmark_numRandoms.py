e3d9fb8b66e9e108ba4852d06c47e8ebab0bc289
Louie Sinadjan <louie_sinz@yahoo.co.uk>
2025-04-23 20:06:56 +0100

    results

--- cuda/benchmark_numRandoms.py
+++ cuda/benchmark_numRandoms.py
@@ -10,19 +10,28 @@ def run_and_time(command):
 
 def benchmark(executable, runs, numRandoms, writer):
     for i in range(1, runs + 1):
-        duration = run_and_time(f"./{executable} -l 400 -x true -r {numRandoms}")
+        duration = run_and_time(f"./{executable} -l 400 -x true -R {numRandoms}")
         writer.writerow([numRandoms, i, f"{duration:.9f}"])
         print(f"{numRandoms},{i},{duration:.9f}")
 
 with open("results_numrandoms.csv", mode="a", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["numRandoms", "run", "time_seconds"])
+    # writer.writerow(["numRandoms", "run", "time_seconds"])
     
-    benchmark("cuda-escg", 10, "1000000", writer)
-    benchmark("cuda-escg", 10, "5000000", writer)
-    benchmark("cuda-escg", 10, "10000000", writer)
-    benchmark("cuda-escg", 10, "20000000", writer)
-    benchmark("cuda-escg", 10, "30000000", writer)
-    benchmark("cuda-escg", 10, "40000000", writer)
-    benchmark("cuda-escg", 10, "50000000", writer)
+    # benchmark("cuda-escg", 10, "1000000", writer)
+    # benchmark("cuda-escg", 10, "5000000", writer)
+    # benchmark("cuda-escg", 10, "10000000", writer)
+    # benchmark("cuda-escg", 10, "20000000", writer)
+    # benchmark("cuda-escg", 10, "30000000", writer)
+    # benchmark("cuda-escg", 10, "40000000", writer)
+    # benchmark("cuda-escg", 10, "50000000", writer)
+    # benchmark("cuda-escg", 10, "100000000", writer)
+    # benchmark("cuda-escg", 10, "250000000", writer)
+    # benchmark("cuda-escg", 10, "500000000", writer)
+
+    benchmark("cuda-escg", 10, "27500000", writer)
+    benchmark("cuda-escg", 10, "32500000", writer)
+
+
+
     
